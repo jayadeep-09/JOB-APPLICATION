a486c12529f4f2d766cc87626ee3a7527f97a45e
jayadeep-09 <jayadeepchalamalasetti9@gmail.com>
2026-06-02 10:53:41 +0530

    updated code

--- dockerfile
+++ dockerfile
@@ -4,10 +4,10 @@ WORKDIR /app
 
 COPY . .
 
-RUN pip install  -r requirements.txt
+RUN pip install --no-cache-dir -r requirements.txt
 
 RUN python manage.py collectstatic --noinput
 
 EXPOSE 10000
 
-CMD ["gunicorn", "job application.wsgi:application", "--bind", "0.0.0.0:10000"]
+CMD ["gunicorn", "job.wsgi:application", "--bind", "0.0.0.0:10000"]
--- job/settings.py
+++ job/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -121,10 +122,19 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
+STORAGES = {
+    'default': {
+        'BACKEND': 'django.core.files.storage.FileSystemStorage',
+    },
+    'staticfiles': {
+        'BACKEND': 'whitenoise.storage.CompressedManifestStaticFilesStorage',
+    },
+}
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = BASE_DIR / 'media'
